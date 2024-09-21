6e0f244d5ab1a18189592692c4c98b45a21c59cf
ad-itya07 <jhaaditya2207@gmail.com>
2024-09-21 11:34:32 +0530

    updated proxy settings

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/': {
+      '/api': {
         target: 'http://localhost:5000', // Your backend server URL
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
