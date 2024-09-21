d0721b1b50a2b9d8f93ee909eb8a2606b2155591
ad-itya07 <jhaaditya2207@gmail.com>
2024-09-21 13:36:13 +0530

    updated by using table component

--- frontend/src/Components/contactlist.jsx
+++ frontend/src/Components/contactlist.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import Contact from "./Contact";
+import Table from "./table";
 import "../ContactList.css";
 
 const apiUrl = import.meta.env.VITE_API_URL;
@@ -56,37 +56,14 @@ function ContactList() {
 
   const handlePageChange = (page) => {
     setPage(page);
-
   };
 
   return (
     <div className="container">
-      <h2>Contact List : P{page}</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Email</th>
-            <th>Phone</th>
-            <th>Job Title</th>
-            {/* Add more columns if needed */}
-          </tr>
-        </thead>
-        <tbody>
-          {contacts.map((contact) => (
-            <Contact
-              key={contact.id}
-              firstName={contact.firstName}
-              lastName={contact.lastName}
-              email={contact.email}
-              phone={contact.phone}
-              jobTitle={contact.jobTitle}
-            />
-          ))}
-        </tbody>
-      </table>
+        <h2>Contact List : P{page}</h2>
+        <Table contacts={contacts} />
 
+        
       {/* Pagination Controls */}
       <div className="pagination">
         {/* METHOD 2 BELOW */}
@@ -118,7 +95,7 @@ function ContactList() {
         >{`<`}</button>
 
         {getPageNumbers().map((pageNumber) => (
-          <button key={pageNumber} onClick={() => handlePageChange(pageNumber)} >
+          <button key={pageNumber} onClick={() => handlePageChange(pageNumber)}>
             {pageNumber}
           </button>
         ))}
