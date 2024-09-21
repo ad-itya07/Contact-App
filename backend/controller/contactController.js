35a8b3b1e16847cf8b43e5905d315c5b154e024e
ad-itya07 <jhaaditya2207@gmail.com>
2024-09-21 16:13:39 +0530

    added filtering

--- backend/controller/contactController.js
+++ backend/controller/contactController.js
@@ -4,18 +4,26 @@ const prisma = new PrismaClient();
 
 class ContactController {
   async getContacts(req, res) {
-    let { page, limit } = req.query;
+    let { page, limit, countryCode } = req.query;
     
     page = page ? parseInt(page) : 1;
     limit = limit ? parseInt(limit) : 10;
 
+    const filters = {};
+    if (countryCode) {
+      filters.countryCode = countryCode.value;
+    }
+
     const skip = (page - 1) * limit;
     const contacts = await prisma.contact.findMany({
+      where: filters,
       skip,
       take: parseInt(limit),
     });
 
-    const totalContacts = await prisma.contact.count();
+    const totalContacts = await prisma.contact.count({
+      where: filters,
+    });
     const totalPages = Math.ceil(totalContacts / limit);
 
     res.json({
